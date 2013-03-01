8549c8d171e5e7ee30dbee943637a28fba747ae9
karim hamidou <khamidou@gmail.com>
2013-03-01 20:06:35 +0100

    render using template

--- grid.py
+++ grid.py
@@ -3,6 +3,14 @@ import os
 import yaml
 import jinja2
 import distutils.dir_util
+import shutil
+
+def render_post(post_metadata):
+
+    if post_metadata["type"] == "image":
+        return "<img src='%s'></img>" % post_metadata["source"]
+    elif post_metadata["type"] == "post":
+        return post_metadata["title"]
 
 def process_post(blog_config, file):
     template = env.get_template('post.html')
@@ -18,6 +26,10 @@ def process_post(blog_config, file):
     return metadata
 
 
+def process_image(blog_config, file):
+    shutil.copyfile("posts/" + file, "output/" + file)    
+    return {"type": "image", "source": file}
+
 def create_index(blog_config, metadata_list):
     template = env.get_template('index.html')
 
@@ -25,6 +37,7 @@ def create_index(blog_config, metadata_list):
         out.write(template.render(blog_config = blog_config, posts_metadata = metadata_list))
 
 env = jinja2.Environment(loader=jinja2.PackageLoader('grid', 'templates'))
+env.globals['render_post'] = render_post
 
 if __name__ == "__main__":
     blog_config = None
@@ -48,9 +61,12 @@ if __name__ == "__main__":
 
     print "Generating post pages"
     for file in os.listdir('posts/'):
+
         ext = os.path.splitext(file)[1]
         if ext in [".yaml"] and file[0] != ".":
             metadata_list.append(process_post(blog_config, file))
+        elif ext in [".png", ".jpg", ".jpeg"] and file[0] != ".":
+            metadata_list.append(process_image(blog_config, file))
 
     print "Creating index.html"
     create_index(blog_config, metadata_list)
